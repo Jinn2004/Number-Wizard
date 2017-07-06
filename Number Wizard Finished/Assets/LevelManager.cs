8bc8aa8ac1e4cadccf8eef56695b12898aba6bc4
David Pruitt <davidbpruitt2004@gmail.com>
2017-07-05 18:44:36 -0600

    Finished and corrected with Random
    Gives the game a random number to guess with the binary search so the player may win with one number but lose the next time with the same number.

---
+++ Number Wizard Finished/Assets/LevelManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string Levelname){
+		Debug.Log ("Level load requested for " + Levelname);
+		Application.LoadLevel (Levelname);
+	}
+
+	public void QuitLevel(string Levelname){
+		Debug.Log ("We have called quit level" + Levelname);
+		Application.Quit ();
+	}
+
+	public void Higher(string Levelname){
+		Debug.Log ("We have said it is higher");
+	}
+
+	public void Lower(string Levelname){
+		Debug.Log ("We have said that it is lower.");
+	}
+}
---
+++ Number Wizard Finished/Assets/numWiz.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+	
+public class numWiz : MonoBehaviour {
+	
+	// Declare a variable that will be for the max value
+	int numberMax;
+	
+	// Declare a variable that will be for the min value
+	int numberMin;
+	int ComputerGuess;
+	int guess;
+	// So I can change the number on the screen.
+	public Text text;
+
+	// Use this for initialization
+	void Start() {
+		StartGame ();
+	}
+
+	void StartGame () {
+		numberMax = 10000;
+		numberMin = 0;
+		guess = Random.Range (3000, 7000);
+		ComputerGuess = 0;
+		text.text = guess.ToString();
+		// use crtl + ' for info on any type of thing.
+	}
+
+	void NextGuess() {
+		ComputerGuess++;
+		if (ComputerGuess >= 10) 
+		{
+			Application.LoadLevel ("Win");
+		}
+		else
+		{
+			// Computer guesses again
+			guess = Random.Range (numberMin, numberMax + 1);
+			//guess = (numberMax + numberMin) / 2;
+			text.text = guess.ToString();
+		}
+	}
+
+	public void GuessLower(){
+		numberMax = guess;
+		NextGuess ();
+	}
+
+	public void GuessHigher(){
+		numberMin = guess;
+		NextGuess();
+	}
+
+	public void Correct(){
+		// When the computer wins
+		Application.LoadLevel ("Lose");
+	}
+
+
+
+}
